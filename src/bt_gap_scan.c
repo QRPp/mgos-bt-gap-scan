929e8f9f5ac925173750a18695e2e580f27bb9b5
QRPp <awesome.walrus+github@gmail.com>
2021-12-17 19:07:27 +0000

    ! Work after Bluedroid→Nimble ESP32 migration
    Retry [re]starting BLE scan loop upon failures of mgos_bt_gap_scan().  Primary
    cause was that it now fails before BT stack initialises, which takes about a
    second after booting up.
    
    Also:
    
    + BT.ScanStop RPC method.  Useful before WiFi OTA: now BLE scan is active 80% of
      time, vs. 50% before, and WiFi OTA is super slow with BLE scan on.
    
    + BT.ScanStart RPC method.  Complements the above.
    
    * Default to 5 sec scan runs instead of 60 s prior.  No material change other
      than more promptly stopping upon a BT.ScanStop or mgos_bt_gap_scan_stop().

--- src/bt_gap_scan.c
+++ src/bt_gap_scan.c
@@ -1,44 +1,68 @@
 #include <mgos.h>
 #include <mgos_bt_gap.h>
 #include <mgos_config.h>
+#include <mgos_rpc.h>
 
 #include <mgos-helpers/log.h>
+#include <mgos-helpers/tmr.h>
 
 #include <mgos_bt_gap_scan.h>
 
-struct mgos_bt_gap_scan_opts bgs_opts;
+static struct mgos_bt_gap_scan_opts bgs_opts;
+static mgos_timer_id bgs_tmr = MGOS_INVALID_TIMER_ID;
 
-static void scan_start(int ev, void *ev_data, void *userdata) {
-  if (ev != MGOS_BT_GAP_EVENT_SCAN_STOP) return;
-  return (void) TRY_GT(mgos_bt_gap_scan, &bgs_opts);
-err:
-  mgos_bt_gap_scan_stop();
+#define SCAN_RETRY_S 1
+static void scan_start(void *userdata) {
+  if (mgos_bt_gap_scan(&bgs_opts))
+    bgs_tmr = MGOS_INVALID_TIMER_ID;
+  else {
+    FNERR(CALL_FAILED_FMT ", wait %u s", "mgos_bt_gap_scan", SCAN_RETRY_S);
+    MGOS_TMR_SET(bgs_tmr, SCAN_RETRY_S * 1000, 0, scan_start, NULL);
+  }
+}
+
+static void scan_restart(int ev, void *ev_data, void *userdata) {
+  if (ev == MGOS_BT_GAP_EVENT_SCAN_STOP) scan_start(userdata);
 }
 
 bool mgos_bt_gap_scan_start(const struct mgos_bt_gap_scan_opts *opts) {
+  if (!mgos_sys_config_get_bt_enable()) return false;
   if (opts)
     bgs_opts = *opts;
   else {
-    bgs_opts.duration_ms = 59999;
+    bgs_opts.duration_ms = 5000;
     bgs_opts.active = mgos_sys_config_get_bt_scan_active();
   }
-  TRY_GT(mgos_event_add_group_handler, MGOS_BT_GAP_EVENT_SCAN_STOP, scan_start,
-         NULL);
-  TRY_GT(mgos_bt_gap_scan, &bgs_opts);
+  TRY_RETF(mgos_event_add_handler, MGOS_BT_GAP_EVENT_SCAN_STOP, scan_restart,
+           NULL);
+  scan_start(NULL);
   return true;
-
-err:
-  mgos_bt_gap_scan_stop();
-  return false;
 }
 
 void mgos_bt_gap_scan_stop() {
-  mgos_event_remove_group_handler(MGOS_BT_GAP_EVENT_SCAN_STOP, scan_start,
-                                  NULL);
+  MGOS_TMR_STOP(bgs_tmr);
+  mgos_event_remove_handler(MGOS_BT_GAP_EVENT_SCAN_STOP, scan_restart, NULL);
+}
+
+static void bt_scan_start_handler(struct mg_rpc_request_info *ri, void *cb_arg,
+                                  struct mg_rpc_frame_info *fi,
+                                  struct mg_str args) {
+  mgos_bt_gap_scan_stop();
+  mg_rpc_send_responsef(ri, "%B", mgos_bt_gap_scan_start(NULL));
+}
+
+static void bt_scan_stop_handler(struct mg_rpc_request_info *ri, void *cb_arg,
+                                 struct mg_rpc_frame_info *fi,
+                                 struct mg_str args) {
+  mgos_bt_gap_scan_stop();
+  mg_rpc_send_responsef(ri, NULL);
 }
 
 bool mgos_bt_gap_scan_init() {
-  if (mgos_sys_config_get_bt_enable() && mgos_sys_config_get_bt_scan_loop())
-    mgos_bt_gap_scan_start(NULL);
+  if (mgos_sys_config_get_bt_scan_loop()) mgos_bt_gap_scan_start(NULL);
+  mg_rpc_add_handler(mgos_rpc_get_global(), "BT.ScanStart", "",
+                     bt_scan_start_handler, NULL);
+  mg_rpc_add_handler(mgos_rpc_get_global(), "BT.ScanStop", "",
+                     bt_scan_stop_handler, NULL);
   return true;
 }
